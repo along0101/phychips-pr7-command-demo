da1f509b957d182d97492b97e1beda18e7b660d0
along <tsilone@qq.com>
2025-09-14 14:04:10 +0800

    [IMP] 修改探索方法

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import time
 # Command: preamble(0xCC) + header(2 Bytes) + payload length(1-2 Bytes) + payload(N Bytes) + checksum(1 Byte)
 class SerialReader:
     def __init__(self, port="/dev/ttyUSB0", baudrate=460800):
+        self.port = port
+        self.baudrate = baudrate
         self.ser = serial.Serial(port, baudrate)
 
     # 从头到尾全部相加，如果大于0xff，取头尾字节相加，知道全部处理完毕。然后取他们的反码。最后如果遇到0xcc或者0xaa，减1.例如0xCC变成0xCB，0xAA变成0xA9
@@ -29,23 +31,34 @@ class SerialReader:
     def info(self):
         # 12 << 0x00 & 0x101
         header = 0x101
-        # product code: 0x01, serial number: 0x02, firmware version: 0x03
-        command = [0xCC, header >> 8 & 0xFF, header & 0xFF, 0x01, 0x03]
+        # product code: 0x01, serial number: 0x02, firmware version: 0x03, max power: 0x07
+        command = [0xCC, header >> 8 & 0xFF, header & 0xFF, 0x01, 0x02]
         checksum = self.__checksum(bytearray(command))
         command.append(checksum)
         self.ser.write(bytearray(command))
-        time.sleep(1)
+        time.sleep(0.08)  # 延迟，准备数据
         resp = self.ser.read_all()
         # resp = self.ser.read(100)
         print(resp)
+        # print("checksum:", hex(resp[-1]), hex(self.__checksum(resp[0:-1])))
         if resp and len(resp) > 5 and resp[-1] == self.__checksum(resp[0:-1]):
-            print("payload length:", resp[3])
+            print("payload length:", hex(resp[3]))
             print("error code:", resp[4])
-            print("payload:", resp[5:-1])
             # print("payload:", resp[5 : (5 + 16 - 1)])
+            print("payload:", resp[5:-1])
+            print("id:", hex(resp[5]))
+            print("data length:", hex(resp[6]))
+            print("data:", resp[7:-1].decode())
+
+    def connect(self):
+        if self.ser is None:
+            self.ser = serial.Serial(self.port, self.baudrate)
+        else:
+            self.ser.open()
 
     def disconnect(self):
         self.ser.close()
+        self.ser = None
 
 
 if __name__ == "__main__":
