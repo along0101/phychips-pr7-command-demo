4220355de7e67928b4a1e3d63c0c6044e2901fec
along <tsilone@qq.com>
2025-09-21 16:49:17 +0800

    [IMP] 添加探索方法

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # phychips-pr7-command-demo
+
+
+### checksum in c/c++
+```c++
+unsigned char RcpApi::calculateChecksum(unsigned char *message, unsigned short length)
+{
+	unsigned short checksum = 0;
+
+	for(int i = 0; i < length; i++)
+	{
+		checksum += message[i];
+		checksum += (checksum >> 8);
+		checksum &= 0xff;
+	}
+
+	if( (checksum == (~(PREAMBLE_COMMAND) & 0xff) ) | (checksum == (~(PREAMBLE_RESPONSE) & 0xff) ) )		//If same as preamble (0xCC or 0xAA)
+		checksum += 1;
+
+	checksum = ~checksum & 0xff;
+
+	return (unsigned char) checksum;
+}
+```
--- main.py
+++ main.py
@@ -14,6 +14,9 @@ class SerialReader:
     def __checksum(self, buffer: bytearray):
         val = 0x00
         for byte in buffer:
+            # val += byte
+            # val += (val >> 8)
+            # val &= 0xFF
             val = val + byte
             if val > 0xFF:
                 val = (val >> 8 & 0xFF) + (val & 0xFF)
@@ -29,7 +32,6 @@ class SerialReader:
         print(hex(val), val == 0x90)
 
     def info(self):
-        # 12 << 0x00 & 0x101
         header = 0x101
         # product code: 0x01, serial number: 0x02, firmware version: 0x03, max power: 0x07
         command = [0xCC, header >> 8 & 0xFF, header & 0xFF, 0x01, 0x02]
@@ -50,6 +52,81 @@ class SerialReader:
             print("data length:", hex(resp[6]))
             print("data:", resp[7:-1].decode())
 
+    def region(self):
+        header = 0x119
+        command = [0xCC, header >> 8 & 0xFF, header & 0xFF, 0x00]
+        checksum = self.__checksum(bytearray(command))
+        command.append(checksum)
+        self.ser.write(bytearray(command))
+        time.sleep(0.08)  # 延迟，准备数据
+        resp = self.ser.read_all()
+        print(resp)
+        # print("checksum:", hex(resp[-1]), hex(self.__checksum(resp[0:-1])))
+        if resp and len(resp) > 5 and resp[-1] == self.__checksum(resp[0:-1]):
+            print("payload length:", hex(resp[3]))
+            print("error code:", resp[4])
+            print("payload:", resp[5:-1])
+
+    def set_region(self, code: int):
+        header = 0x01 << 12 | 0x119
+        command = [0xCC, header >> 8 & 0xFF, header & 0xFF, 0x01, code & 0xFF]
+        checksum = self.__checksum(bytearray(command))
+        command.append(checksum)
+        self.ser.write(bytearray(command))
+        time.sleep(0.08)  # 延迟，准备数据
+        resp = self.ser.read_all()
+        print(resp)
+        # print("checksum:", hex(resp[-1]), hex(self.__checksum(resp[0:-1])))
+        if resp and len(resp) > 5 and resp[-1] == self.__checksum(resp[0:-1]):
+            print("payload length:", hex(resp[3]))
+            print("error code:", resp[4])
+            print("payload:", resp[5:-1])
+
+    def mode(self):
+        header = 0x00 << 12 | 0x155
+        command = [0xCC, header >> 8 & 0xFF, header & 0xFF, 0x00]
+        checksum = self.__checksum(bytearray(command))
+        command.append(checksum)
+        self.ser.write(bytearray(command))
+        time.sleep(0.08)  # 延迟，准备数据
+        resp = self.ser.read_all()
+        print(resp)
+        # print("checksum:", hex(resp[-1]), hex(self.__checksum(resp[0:-1])))
+        if resp and len(resp) > 5 and resp[-1] == self.__checksum(resp[0:-1]):
+            print("payload length:", hex(resp[3]))
+            print("error code:", resp[4])
+            print("payload:", resp[5:-1], hex(ord(resp[5:-1])))
+
+    def access_mode(self):
+        header = 0x00 << 12 | 0x170
+        command = [0xCC, header >> 8 & 0xFF, header & 0xFF, 0x00]
+        checksum = self.__checksum(bytearray(command))
+        command.append(checksum)
+        self.ser.write(bytearray(command))
+        time.sleep(0.08)  # 延迟，准备数据
+        resp = self.ser.read_all()
+        print(resp)
+        # print("checksum:", hex(resp[-1]), hex(self.__checksum(resp[0:-1])))
+        if resp and len(resp) > 5 and resp[-1] == self.__checksum(resp[0:-1]):
+            print("payload length:", hex(resp[3]))
+            print("error code:", resp[4])
+            print("payload:", resp[5:-1], hex(ord(resp[5:-1])))
+
+    def read_mem(self, bank: int):
+        header = 0x01 << 12 | 0x220
+        command = [0xCC, header >> 8 & 0xFF, header & 0xFF, 0x01, bank & 0xFF]
+        checksum = self.__checksum(bytearray(command))
+        command.append(checksum)
+        self.ser.write(bytearray(command))
+        time.sleep(0.08)  # 延迟，准备数据
+        resp = self.ser.read_all()
+        print(resp)
+        # print("checksum:", hex(resp[-1]), hex(self.__checksum(resp[0:-1])))
+        if resp and len(resp) > 5 and resp[-1] == self.__checksum(resp[0:-1]):
+            print("payload length:", hex(resp[3]))
+            print("error code:", resp[4])
+            print("payload:", resp[5:-1])
+
     def connect(self):
         if self.ser is None:
             self.ser = serial.Serial(self.port, self.baudrate)
@@ -65,5 +142,11 @@ if __name__ == "__main__":
     # 8N1
     reader = SerialReader("/dev/tty.usbserial-0001")
     # reader.test()
-    reader.info()
+    # reader.info()
+    # reader.set_region(0x07)
+    # time.sleep(0.1)
+    # reader.region()
+    # reader.mode()
+    # reader.access_mode()
+    reader.read_mem(0x00)
     reader.disconnect()
